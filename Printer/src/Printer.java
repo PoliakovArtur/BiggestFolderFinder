76d6c2d71b7b1ada030b52aefb478c43eea5c9e7
Artur Poliakov <masjro@mail.ru>
2021-11-25 13:36:13 +0300

    Модуль 4. Задание 3

---
+++ Printer/src/Printer.java
@@ -0,0 +1,43 @@
+public class Printer {
+
+    public String queue;
+    public int amountPages;
+    public static int amountPagesAllTime = 0;
+
+    public void append(String text, String name, int amountPages) {
+        this.amountPages = amountPages;
+        queue = name + "\n" + text;
+    }
+
+    public void append(String text, String name) {
+        append(text, name, 1);
+    }
+
+    public void append(String text) {
+        append(text, "", 1);
+    }
+
+    public void append(String text, int amountPages) {
+        append(text, "", amountPages);
+    }
+
+    public int getPendingPagesCount() {
+        return amountPages;
+    }
+
+    public int getPrintPagesCountAllTime () {
+        return amountPagesAllTime;
+    }
+
+    public void print() {
+        amountPagesAllTime = amountPagesAllTime + amountPages;
+        System.out.println(queue);
+        queue = "";
+        amountPages = 0;
+    }
+
+    public void clear() {
+        queue = "";
+        amountPages = 0;
+    }
+}
---
+++ Printer/src/main.java
@@ -0,0 +1,22 @@
+public class main {
+    public static void main(String[] args) {
+        Printer textOne = new Printer();
+        textOne.append("Работают белазы", "ВНИМАНИЕ!", 6);
+        System.out.println("\nКоличество страниц " + textOne.getPendingPagesCount());
+        textOne.print();
+
+        Printer textTwo = new Printer();
+        textTwo.append("Окрашено");
+        System.out.println("\nКоличество страниц " + textTwo.getPendingPagesCount());
+        textTwo.print();
+
+        Printer textThree = new Printer();
+        textThree.append("Не входить!", 3);
+        textThree.clear();
+        textThree.append("Не входить!", "Внимание!");
+        System.out.println("\nКоличество страниц " + textThree.getPendingPagesCount());
+        textThree.print();
+
+        System.out.println("\nКоличество страниц за все время работы принтера " + textOne.getPrintPagesCountAllTime());
+    }
+}
