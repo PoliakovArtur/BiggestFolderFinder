a22c8e7855cc5e107308079c67c52e0edf1afa5a
Artur Poliakov <masjro@mail.ru>
2021-11-26 12:08:25 +0300

    5.3 Домашняя работа 5.1. Инкапсуляция

--- Elevator/src/Elevator.java
+++ Elevator/src/Elevator.java
@@ -4,7 +4,7 @@ public class Elevator {
     private int minFloor;
     private int maxFloor;
 
-    Elevator(int minFloor, int maxFloor ) {
+    Elevator(int minFloor, int maxFloor) {
         this.minFloor = minFloor;
         this.maxFloor = maxFloor;
     }
@@ -23,24 +23,19 @@ public class Elevator {
 
     public void move(int floor) {
         if (floor > maxFloor || floor < minFloor) {
-            System.out.println("Этаж выбран неверно");
+            System.out.println("Этаж введен неверно");
             return;
         }
 
-        if (floor > currentFloor) {
-            moveUp();
-            System.out.println(currentFloor);
-            move(floor);
-            return;
-        }
+            while (floor > currentFloor) {
+                moveUp();
+                System.out.println(currentFloor);
+            }
 
-        if (floor < currentFloor) {
-            moveDown();
-            System.out.println(currentFloor);
-            move(floor);
-            return;
-        }
-        System.out.println("Вы находитесь на " + getCurrentFloor() + " этаже");
-
+            while (floor < currentFloor) {
+                moveDown();
+                System.out.println(currentFloor);
+            }
+            System.out.println("Вы находитесь на " + currentFloor + " этаже");
     }
 }
