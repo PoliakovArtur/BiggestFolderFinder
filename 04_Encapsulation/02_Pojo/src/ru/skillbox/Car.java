d1c0d23a3621d27d6067dfdc0644109a1496cad7
Artur Poliakov <masjro@mail.ru>
2021-11-26 18:19:36 +0300

    Домашняя работа 5.2. POJO-классы, геттеры и сеттеры

---
+++ 04_Encapsulation/02_Pojo/src/ru/skillbox/Car.java
@@ -0,0 +1,48 @@
+package ru.skillbox;
+
+public class Car {
+
+    private String brand;
+    private String model;
+    private String color;
+    private int year;
+
+    public Car(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
+
--- 04_Encapsulation/02_Pojo/src/ru/skillbox/Main.java
+++ 04_Encapsulation/02_Pojo/src/ru/skillbox/Main.java
@@ -3,5 +3,27 @@ package ru.skillbox;
 public class Main {
 
     public static void main(String[] args) {
+        Country russia = new Country("Россия");
+        russia.setArea(17130000);
+        russia.setPopulation(144100000);
+        russia.setCapital("Москва");
+        russia.setAccessToTheSea(true);
+
+        System.out.println("Страна " + russia.getName());
+        System.out.println("Cтолица " + russia.getCapital());
+        System.out.println("Население " + russia.getPopulation() + " человек");
+        System.out.println("Площадь " + russia.getArea() + " кв.км");
+        System.out.println(russia.isAccessToTheSea() ? "Есть выход к морю" : "Нет выхода к морю");
+
+        System.out.println("\n=========================================\n");
+
+        Car volkswagen = new Car("Volkswagen", "Polo");
+        volkswagen.setColor("Черный");
+        volkswagen.setYear(2016);
+
+        System.out.println("Бренд " + volkswagen.getBrand());
+        System.out.println("Модель " + volkswagen.getModel());
+        System.out.println("Год выпуска " + volkswagen.getYear());
+        System.out.println("Цвет " + volkswagen.getColor());
     }
 }
