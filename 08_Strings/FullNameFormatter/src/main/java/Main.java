e849b51d31706c349010059b925dfe766a8f0c7c
Artur Poliakov <masjro@mail.ru>
2021-12-28 10:50:38 +0300

    9.10 Домашняя работа Задания 1 и 2

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -2,17 +2,43 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().trim();
+        System.out.println(getPersonInfo(input));
+
     }
-  }
 
+    public static String getPersonInfo(String input) {
+        int spaceCount = 0;
+        String errorMessage = "Введенная строка не является ФИО";
+
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isDigit(input.charAt(i))) {
+                return errorMessage;
+            }
+            if (input.charAt(i) == ' ') {
+                spaceCount++;
+            }
+        }
+
+        if(spaceCount != 2) {
+            return errorMessage;
+        }
+
+        int end = input.indexOf(' ');
+        String surname = input.substring(0, end);
+
+        int start = end + 1;
+        end = input.lastIndexOf(' ');
+        String name = input.substring(start, end);
+
+        start = input.lastIndexOf(' ') + 1;
+        String patronymic = input.substring(start);
+
+        return  "Фамилия: " + surname + System.lineSeparator() +
+                "Имя: " + name + System.lineSeparator() +
+                "Отчество: " + patronymic;
+    }
 }
--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,28 @@
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
-  }
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        String start1 = "заработал";
+        String start2 = "Петя -";
+        String start3 = "Маша -";
+        String end = "руб";
 
+        int startIndex = text.indexOf(start1) + start1.length();
+        int endIndex = text.indexOf(end);
+        String money = text.substring(startIndex, endIndex).trim();
+        int sum = Integer.parseInt(money);
+
+        startIndex = text.indexOf(start2) + start2.length();
+        endIndex = text.indexOf(end, startIndex);
+        money = text.substring(startIndex, endIndex).trim();
+        sum += Integer.parseInt(money);
+
+        startIndex = text.indexOf(start3) + start3.length();
+        endIndex = text.lastIndexOf(end);
+        money = text.substring(startIndex, endIndex).trim();
+        sum += Integer.parseInt(money);
+
+        System.out.println(sum);
+    }
 }
