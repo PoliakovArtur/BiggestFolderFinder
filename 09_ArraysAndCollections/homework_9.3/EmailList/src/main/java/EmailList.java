e9a3beebf58257331358243b2203bbcdb4f0c3e2
Artur Poiakov <masjro@mail.ru>
2022-01-08 10:50:48 +0300

    10.9 Домашняя работа. Множества - Set

--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
@@ -4,7 +4,6 @@ public class EmailList {
     private TreeSet<String> emailList = new TreeSet<>();
     private final static String LETTERS_LIST = "[1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]+";
     private final static String REGEX = LETTERS_LIST + '@' + LETTERS_LIST + "\\." + LETTERS_LIST;
-
     public void add(String email) {
         if (!email.matches(REGEX)) {
             System.out.println(Main.WRONG_EMAIL_ANSWER);
@@ -13,7 +12,8 @@ public class EmailList {
         emailList.add(email.toLowerCase(Locale.ROOT));
     }
 
-    public Set<String> getSortedEmails() {
-        return emailList;
+    public List<String> getSortedEmails() {
+        ArrayList<String> sortedEmails = new ArrayList<>(emailList);
+        return Collections.unmodifiableList(sortedEmails);
     }
 }
--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
@@ -2,20 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
-    
-    /* TODO:
-        Пример вывода списка Email, после ввода команды LIST в консоль:
-        test@test.com
-        hello@mail.ru
-        - каждый адрес с новой строки
-        - список должен быть отсортирован по алфавиту
-        - email в разных регистрах считается одинаковыми
-           hello@skillbox.ru == HeLLO@SKILLbox.RU
-        - вывод на печать должен быть в нижнем регистре
-           hello@skillbox.ru
-        Пример вывода сообщения об ошибке при неверном формате Email:
-        "Неверный формат email"
-    */
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -34,10 +20,9 @@ public class Main {
                 case "ADD" -> {
                     if (split.length == 1) {
                         System.out.println(WRONG_EMAIL_ANSWER);
+                        break;
                     }
-                    else {
                         emailList.add(split[1]);
-                    }
                 }
                 case "LIST" -> {
                     for (String email : emailList.getSortedEmails()) {
