2e6b2eaf8e4dc4bf9233dab41e1fcfe18095643a
Artur Poiakov <masjro@mail.ru>
2022-01-21 13:12:14 +0300

    11.11 Домашняя работа №4

--- AdvancedOOPFeatures/homework_1/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_1/src/main/java/Main.java
@@ -13,14 +13,11 @@ public class Main {
     }
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
-        Collections.sort(staff, (o1, o2) -> {
-        return o1.getSalary().compareTo(o2.getSalary());
-        });
-        Collections.sort(staff, (o1, o2) -> {
-            if (!o1.getSalary().equals(o2.getSalary())) {
-                return 0;
+        staff.sort((o1, o2) -> {
+            if (o1.getSalary().equals(o2.getSalary())) {
+                return o1.getName().compareTo(o2.getName());
             }
-            return o1.getName().compareTo(o2.getName());
+            return o1.getSalary().compareTo(o2.getSalary());
         });
     }
 }
