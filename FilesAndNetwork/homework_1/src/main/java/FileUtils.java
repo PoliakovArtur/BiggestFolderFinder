3fed2f984739addfd210bbbb7cb07680bc37d496
Artur Poiakov <masjro@mail.ru>
2022-02-09 23:30:56 +0300

    15.5 Практическая работа №1

--- FilesAndNetwork/homework_1/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_1/src/main/java/FileUtils.java
@@ -1,6 +1,28 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class FileUtils {
 
-    public static long calculateFolderSize(String path) {
-        return 0;
+    public static long calculateFolderSize(String path) throws Exception {
+        File folder = new File(path);
+        if (!folder.exists()) {
+            throw new Exception("Неверный путь");
+        }
+        return getFolderSize(folder);
+    }
+
+    private static long getFolderSize(File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for(File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
--- FilesAndNetwork/homework_1/src/main/java/Main.java
+++ FilesAndNetwork/homework_1/src/main/java/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            try {
+                System.out.println("Введите путь до папки");
+                String path = scanner.nextLine();
+                long size = FileUtils.calculateFolderSize(path);
+                String[] units = {"B", "KB", "MB", "GB", "TB"};
+
+                for (int rate = 0; rate < 50; rate+=10) {
+                    if ((size >= Math.pow(2, rate)) && (size < Math.pow(2, rate + 10))) {
+                        double result = size / Math.pow(2, rate);
+                        String format = "Размер папки " + path + " составляет %.2f %s\n";
+                        System.out.printf(format, result, units[rate / 10]);
+                    }
+                }
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
     }
 }
