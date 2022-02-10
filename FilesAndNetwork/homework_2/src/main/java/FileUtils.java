f1dbd63a11d7c935e231e02b3d0f202ac64e6cb3
Artur Poiakov <masjro@mail.ru>
2022-02-11 00:25:46 +0300

    Практическая работа №2

--- FilesAndNetwork/homework_2/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_2/src/main/java/FileUtils.java
@@ -1,5 +1,30 @@
+import javax.swing.text.AttributeSet;
+import javax.swing.text.html.Option;
+import java.io.*;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+
+    public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
+        copyFiles(new File(sourceDirectory).listFiles(), destinationDirectory);
+    }
+
+    public static void copyFiles(File[] files, String destinationDirectory) throws IOException {
+
+        for (int i = 0; i < files.length; i++) {
+            String newPath = destinationDirectory + "\\" + files[i].getName();
+            if (files[i].isFile()) {
+                Files.copy(Paths.get(files[i].getPath()), Paths.get(newPath), StandardCopyOption.REPLACE_EXISTING);
+            }
+            else {
+                Files.createDirectory(Paths.get(newPath));
+                copyFiles(files[i].listFiles(), newPath);
+            }
+        }
     }
 }
+
+
--- FilesAndNetwork/homework_2/src/main/java/Main.java
+++ FilesAndNetwork/homework_2/src/main/java/Main.java
@@ -1,4 +1,37 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+                System.out.println("Введите путь до папки, которую нужно скопировать");
+                String sourceDirectory = scanner.nextLine();
+
+                if (!(new File(sourceDirectory).exists())) {
+                    throw new IOException("Неверно указан путь до копируемой папки");
+                }
+
+                System.out.println("Введите путь до папки, в которую будет произведено копирование");
+                String destinationDirectory = scanner.nextLine();
+
+                if (!(new File(destinationDirectory).exists())) {
+                    throw new IOException("Неверно указан путь до папки, в которую будут скопированы файлы");
+                }
+
+                FileUtils.copyFolder(sourceDirectory, destinationDirectory);
+
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+
     }
 }
